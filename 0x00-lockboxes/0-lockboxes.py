ff712e9decd6709233081dcc3a6ac3bfec87d9b0
Julian Franco Rua <julianfrancor@gmail.com>
2020-12-09 17:40:47 -0500

    fixing pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,30 +4,30 @@
 """
 
 def canUnlockAll(boxes):
-  """
-    canUnlockAll is a method that checks whether
-    a box can be opened or not
+    """
+      canUnlockAll is a method that checks whether
+      a box can be opened or not
 
-    Args: boxes -> list of lists
-    return:
-      True if all the boxes could be opened
-      False if all the boxes couldn't be opened
-  """
-  unlocked = [False] * len(boxes)
-  unlocked[0] = True
-  keys = []
+      Args: boxes -> list of lists
+      return:
+        True if all the boxes could be opened
+        False if all the boxes couldn't be opened
+    """
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    keys = []
 
-  for key in boxes[0]:
-    keys.append(key)
+    for key in boxes[0]:
+      keys.append(key)
 
-  for key in keys:
-    if ((key < len(boxes)) and (unlocked[key] is False)):
-      unlocked[key] = True
-      for i in boxes[key]:
-        if i not in keys:
-          keys.append(i)
+    for key in keys:
+      if ((key < len(boxes)) and (unlocked[key] is False)):
+        unlocked[key] = True
+        for i in boxes[key]:
+          if i not in keys:
+            keys.append(i)
 
-  if False in unlocked:
-    return False
-  else:
-    return True
+    if False in unlocked:
+      return False
+    else:
+      return True
